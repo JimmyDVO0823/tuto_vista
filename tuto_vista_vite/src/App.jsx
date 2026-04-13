abce1c2d0752d199d3c484529d18856f2f7cdd70
JimmyDVO0823 <jimmyvelandia@outlook.es>
2026-04-12 20:16:15 -0500

    el botón de sign in sirve

--- tuto_vista_vite/src/App.jsx
+++ tuto_vista_vite/src/App.jsx
@@ -1,11 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Home from './pages/Home'
 
-
 function App() {
   return (
-    //<Login />
-    <Home />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   )
 }
 
