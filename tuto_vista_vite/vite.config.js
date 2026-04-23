c87ae1eaeec9607cf9969371c464c2ea4ea33db1
JimmyDVO0823 <jimmyvelandia@outlook.es>
2026-04-23 17:27:18 -0500

    se hacen cambios para que al hacer deploy se publque en pages

--- tuto_vista_vite/vite.config.js
+++ tuto_vista_vite/vite.config.js
@@ -12,6 +12,7 @@ const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(file
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
+  base: '/tuto_vista/',
   plugins: [react(), tailwindcss()],
   server: {
     allowedHosts: true // Esto permite cualquier enlace de Localtunnel
