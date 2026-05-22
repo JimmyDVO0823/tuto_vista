a02ce61e8254318ca233258dca89dc85714a5c5b
JimmyDVO0823 <jimmyvelandia@outlook.es>
2026-05-22 09:12:26 -0500

    juan3 el hijo prodigo y elegido

--- tuto_vista_vite/src/lib/api.js
+++ tuto_vista_vite/src/lib/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'http://localhost:8081';
 
 const handleApiError = async (response) => {
   const errorData = await response.json().catch(() => ({}));
