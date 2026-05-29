d09671827a1ab976e7cc7c9024203db9e6be4f1e
JimmyDVO0823 <jimmyvelandia@outlook.es>
2026-05-29 18:54:04 -0500

    se hace el cambio de la variable para los mensajes en tiempo real

--- tuto_vista_vite/vite.config.js
+++ tuto_vista_vite/vite.config.js
@@ -13,6 +13,9 @@ const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(file
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   base: '/tuto_vista/',
+  define: {
+    global: 'window',
+  },
   plugins: [react(), tailwindcss()],
   server: {
     allowedHosts: true // Esto permite cualquier enlace de Localtunnel
